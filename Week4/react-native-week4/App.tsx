a4ea1efe4dd7c35692b2425a33dc9760672ce66e
Bao44 <tqbao44@gmail.com>
2024-09-13 14:59:47 +0700

    add header screen tiki_Ok

--- Week4/react-native-week4/App.tsx
+++ Week4/react-native-week4/App.tsx
@@ -1,24 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Screen1_e from './src/screen1_e';
-import Screen2_a from './src/screen2_a';
-import EndScreen from './src/endScreen';
-import Screen2_b from './src/screen2_b';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import Screen1_e from "./src/screen1_e";
+import Screen2_a from "./src/screen2_a";
+import EndScreen from "./src/endScreen";
+import Screen2_b from "./src/screen2_b";
+import Tiki_Ok from "./src/tiki_Ok";
+import { useState } from "react";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Screen2_b />
+    // <View style={styles.container}>
+
+    // </View>
+
+    <SafeAreaView style={styles.screen_tiki}>
+      <Tiki_Ok/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  screen_tiki: {
+    flex: 1,
+    backgroundColor: "#C4C4C4",
+    paddingTop: Platform.OS === "android" ? 40 : 0,
   },
 });
---
+++ Week4/react-native-week4/src/tiki_Ok.jsx
@@ -0,0 +1,150 @@
+import {
+  Alert,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+
+const Tiki_Ok = () => {
+  const [count, setCount] = useState(0);
+
+  function inCrease() {
+    setCount(count + 1);
+  }
+  function reduce() {
+    setCount(count - 1);
+  }
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.header}>
+        <View>
+          <View style={{ flexDirection: "row" }}>
+            <Image source={require("../assets/book.png")} />
+            <View style={styles.headerText}>
+              <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                Nguyên hàm tích phân và ứng dụng
+              </Text>
+              <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                Cung cấp bởi Tiki Trading
+              </Text>
+              <Text style={{ fontSize: 22, fontWeight: "bold", color: "red" }}>
+                141.800 đ
+              </Text>
+              <Text style={styles.textLine}>141.000 đ</Text>
+              <View>
+                <View style={{ flexDirection: "row" }}>
+                  <View style={styles.viewButton}>
+                    <TouchableOpacity style={styles.button} onPress={reduce}>
+                      <Text style={styles.textButton}>-</Text>
+                    </TouchableOpacity>
+                    <Text style={{ fontSize: 25 }}>{count}</Text>
+                    <TouchableOpacity style={styles.button} onPress={inCrease}>
+                      <Text style={styles.textButton}>+</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.viewTextBuy}>
+                    <TouchableOpacity>
+                      <Text style={{ color: "blue", fontSize: 24 }}>
+                        Mua sau
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View style={{ flexDirection: "row", paddingVertical: 25 }}>
+          <Text style={{ fontSize: 18, fontWeight: "bold", paddingRight: 30 }}>
+            Mã giảm giá đã lưu
+          </Text>
+          <TouchableOpacity>
+            <Text style={{ fontSize: 18, color: "blue" }}>Xem tại đây</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          <View style={styles.view2button}>
+            <TouchableOpacity style={styles.view2button_1}>
+              <View
+                style={{ backgroundColor: "#F2DD1B", width: 40, height: 20 }}
+              ></View>
+              <Text style={styles.text2button}>Mã giảm giá</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{ backgroundColor: "#0A5EB7", width: 130, height: 60 }}
+            >
+              <Text style={styles.text2button_1}>Áp dụng</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Tiki_Ok;
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "white",
+    padding: 20,
+  },
+  headerText: {
+    paddingHorizontal: 20,
+  },
+  textLine: {
+    textDecorationLine: "line-through",
+    fontSize: 18,
+    color: "gray",
+  },
+  viewButton: {
+    flexDirection: "row",
+    width: 130,
+    justifyContent: "space-around",
+  },
+  button: {
+    backgroundColor: "#808080",
+    width: 30,
+    marginTop: 5,
+  },
+  textButton: {
+    fontSize: 24,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  viewTextBuy: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+  view2button: {
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  view2button_1: {
+    borderWidth: 1,
+    width: 220,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  text2button: {
+    fontSize: 22,
+    fontWeight: "bold",
+    paddingLeft: 15,
+  },
+  text2button_1: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    paddingTop: 15,
+  },
+});
