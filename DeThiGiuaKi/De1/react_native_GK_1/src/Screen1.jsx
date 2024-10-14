4f5b71f4391215c1df4744f57d2bf07f5bab7bc6
Bao44 <tqbao44@gmail.com>
2024-10-14 23:04:04 +0700

    add form Screen1

--- DeThiGiuaKi/De1/react_native_GK_1/src/Screen1.jsx
+++ DeThiGiuaKi/De1/react_native_GK_1/src/Screen1.jsx
@@ -1,14 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Fontisto from "@expo/vector-icons/Fontisto";
+emaillock;
 const Screen1 = () => {
   return (
-    <View>
-      <Text>Screen1</Text>
+    <View style={{ paddingVertical: 30 }}>
+      <AntDesign name="arrowleft" size={24} color="gray" />
+      <View style={styles.center}>
+        <Image
+          source={require("../assets/Data/icon.png")}
+          style={{ width: 200, height: 200 }}
+        />
+        <Text>Hello Again!</Text>
+        <Text>Log into your account</Text>
+        <View>
+          <View style={styles.viewInputEmail}>
+            <MaterialCommunityIcons
+              name="email-outline"
+              size={24}
+              color="gray"
+            />
+            <TextInput placeholder="Enter your email address" />
+          </View>
+          <View style={styles.viewInputPass}>
+            <Fontisto name="locked" size={24} color="gray" />
+            <TextInput placeholder="Enter your email address" />
+            <MaterialCommunityIcons name="eye-outline" size={24} color="gray" />
+          </View>
+          <Text>Forgot Password?</Text>
+        </View>
+        <TouchableOpacity>
+          <Text>Continue</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.footer}>
+        <View>
+          <Text>____________________</Text>
+          <Text>or</Text>
+          <Text>____________________</Text>
+        </View>
+        <View>
+          <Image source={require("../assets/Data/google.png")} />
+          <Image source={require("../assets/Data/face.png")} />
+          <Image source={require("../assets/Data/apple.png")} />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Screen1
+export default Screen1;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
