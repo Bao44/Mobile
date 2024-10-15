2b661f69ad44132e76767df7912e14dbaace032b
Bao44 <tqbao44@gmail.com>
2024-10-15 22:18:03 +0700

    add UI screen 2

--- DeThiGiuaKi/De2/react_native_GK2/src/Screen2.jsx
+++ DeThiGiuaKi/De2/react_native_GK2/src/Screen2.jsx
@@ -1,14 +1,137 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Fontisto from "@expo/vector-icons/Fontisto";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
-const Screen2 = () => {
+const Screen2 = ({ navigation }) => {
   return (
     <View>
-      <Text>Screen2</Text>
+      <View style={{ marginTop: 80 }}>
+        <Image
+          source={require("../assets/Data/Image 19.png")}
+          style={{ alignSelf: "center" }}
+        />
+        <Text style={styles.textHeader}>Nice to see you!</Text>
+        <Text style={styles.textDesciption}>Create your account</Text>
+      </View>
+      <View style={{ marginVertical: 20, marginHorizontal: 30, marginTop: 80 }}>
+        <View style={styles.viewInputUser}>
+          <AntDesign
+            name="user"
+            size={24}
+            color="gray"
+            style={{ paddingHorizontal: 10 }}
+          />
+          <TextInput
+            placeholder="Enter your user name"
+            // value={userName}
+            // onChangeText={(newUserName) => setUserName(newUserName)}
+            style={{ paddingRight: 180, paddingLeft: 5 }}
+          />
+        </View>
+        <View style={styles.viewInputEmail}>
+          <MaterialCommunityIcons
+            name="email-outline"
+            size={24}
+            color="gray"
+            style={{ paddingHorizontal: 10 }}
+          />
+          <TextInput
+            placeholder="Enter your email address"
+            // value={email}
+            // onChangeText={(newEmail) => setEmail(newEmail)}
+            style={{ paddingRight: 180, paddingLeft: 5 }}
+          />
+        </View>
+        <View style={styles.viewInputPass}>
+          <Fontisto
+            name="locked"
+            size={24}
+            color="gray"
+            style={{ paddingHorizontal: 10 }}
+          />
+          <TextInput
+            placeholder="Enter your password"
+            // value={password}
+            // onChangeText={(newPassword) => setPassword(newPassword)}
+            style={{ paddingRight: 180, paddingLeft: 5 }}
+          />
+          <MaterialCommunityIcons name="eye-outline" size={24} color="gray" />
+        </View>
+
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <AntDesign name="checksquare" size={24} color="blue" />
+          <Text> I agree with</Text>
+          <Text style={{ color: "blue" }}> Terms & Conditions</Text>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={styles.buttonSignUp}
+        onPress={() => navigation.navigate("Screen3")}
+      >
+        <Text style={styles.textButton}>Continue</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default Screen2
+export default Screen2;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  textHeader: {
+    fontSize: 40,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginVertical: 20,
+  },
+  textDesciption: {
+    fontSize: 20,
+    color: "gray",
+    textAlign: "center",
+  },
+  viewInputUser: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "gray",
+    paddingVertical: 12,
+    borderRadius: 15,
+  },
+  viewInputEmail: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "gray",
+    paddingVertical: 12,
+    borderRadius: 15,
+    marginVertical: 20,
+  },
+  viewInputPass: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "gray",
+    paddingVertical: 12,
+    borderRadius: 15,
+    marginBottom: 30,
+  },
+  buttonSignUp: {
+    backgroundColor: "#00BDD6",
+    width: 400,
+    height: 60,
+    borderRadius: 15,
+    marginTop: 40,
+    alignSelf: "center",
+  },
+  textButton: {
+    color: "white",
+    fontSize: 22,
+    textAlign: "center",
+    marginTop: 15,
+  },
+});
