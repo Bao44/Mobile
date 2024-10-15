5471d0e1f7100fb8d7b56b46624facf2a7cbb878
Bao44 <tqbao44@gmail.com>
2024-10-15 18:42:41 +0700

    add event Screen1, valid login

--- DeThiGiuaKi/De1/react_native_GK_1/App.tsx
+++ DeThiGiuaKi/De1/react_native_GK_1/App.tsx
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View } from "react-native";
+import Screen1 from "./src/Screen1";
+import Screen2 from "./src/Screen2";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Screen1"
+          component={Screen1}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Screen2"
+          component={Screen2}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
--- DeThiGiuaKi/De1/react_native_GK_1/src/Screen1.jsx
+++ DeThiGiuaKi/De1/react_native_GK_1/src/Screen1.jsx
@@ -6,12 +6,29 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import Fontisto from "@expo/vector-icons/Fontisto";
-emaillock;
-const Screen1 = () => {
+import users from "../data/users.json";
+
+const Screen1 = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validEmail = users.map((user) => user.email);
+  const validPassword = users.map((user) => user.password);
+
+  const handleValidation = () => {
+    if (validEmail.includes(email) && validPassword.includes(password)) {
+      navigation.navigate("Screen2");
+    } else {
+      alert("Email or Password is incorrect");
+      setEmail("");
+      setPassword("");
+    }
+  };
+
   return (
     <View style={{ paddingVertical: 30 }}>
       <AntDesign name="arrowleft" size={24} color="gray" />
@@ -20,35 +37,65 @@ const Screen1 = () => {
           source={require("../assets/Data/icon.png")}
           style={{ width: 200, height: 200 }}
         />
-        <Text>Hello Again!</Text>
-        <Text>Log into your account</Text>
-        <View>
+        <Text style={{ fontSize: 40, fontWeight: "bold", marginVertical: 20 }}>
+          Hello Again!
+        </Text>
+        <Text style={{ fontSize: 20, color: "gray" }}>
+          Log into your account
+        </Text>
+        <View style={{ marginVertical: 20 }}>
           <View style={styles.viewInputEmail}>
             <MaterialCommunityIcons
               name="email-outline"
               size={24}
               color="gray"
+              style={{ paddingHorizontal: 10 }}
             />
-            <TextInput placeholder="Enter your email address" />
+            <TextInput
+              placeholder="Enter your email address"
+              value={email}
+              onChangeText={(newEmail) => setEmail(newEmail)}
+              style={{ paddingRight: 180, paddingLeft: 5 }}
+            />
           </View>
           <View style={styles.viewInputPass}>
-            <Fontisto name="locked" size={24} color="gray" />
-            <TextInput placeholder="Enter your email address" />
+            <Fontisto
+              name="locked"
+              size={24}
+              color="gray"
+              style={{ paddingHorizontal: 10 }}
+            />
+            <TextInput
+              placeholder="Enter your password"
+              value={password}
+              onChangeText={(newPassword) => setPassword(newPassword)}
+              style={{ paddingRight: 180, paddingLeft: 5 }}
+            />
             <MaterialCommunityIcons name="eye-outline" size={24} color="gray" />
           </View>
-          <Text>Forgot Password?</Text>
+          <Text style={{ textAlign: "right", color: "#00BDD6" }}>
+            Forgot Password?
+          </Text>
         </View>
-        <TouchableOpacity>
-          <Text>Continue</Text>
+        <TouchableOpacity
+          onPress={handleValidation}
+          style={{
+            backgroundColor: "#00BDD6",
+            paddingHorizontal: 150,
+            paddingVertical: 15,
+            borderRadius: 20,
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 24 }}>Continue</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.footer}>
-        <View>
-          <Text>____________________</Text>
-          <Text>or</Text>
-          <Text>____________________</Text>
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <Text style={styles.textFooter}>____________________</Text>
+          <Text style={[styles.textFooter, { fontSize: 20 }]}>or</Text>
+          <Text style={styles.textFooter}>____________________</Text>
         </View>
-        <View>
+        <View style={styles.buttonFooter}>
           <Image source={require("../assets/Data/google.png")} />
           <Image source={require("../assets/Data/face.png")} />
           <Image source={require("../assets/Data/apple.png")} />
@@ -60,4 +107,36 @@ const Screen1 = () => {
 
 export default Screen1;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  center: {
+    marginTop: 50,
+    alignItems: "center",
+  },
+  viewInputEmail: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "gray",
+    paddingVertical: 12,
+    borderRadius: 15,
+  },
+  viewInputPass: {
+    marginVertical: 20,
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "gray",
+    paddingVertical: 12,
+    borderRadius: 15,
+  },
+  footer: {
+    marginTop: 50,
+  },
+  textFooter: {
+    color: "gray",
+    marginHorizontal: 10,
+  },
+  buttonFooter: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+});
