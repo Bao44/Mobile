895270a9ae39288de06da67fdda6afa20def1bdf
Bao44 <tqbao44@gmail.com>
2024-10-16 21:37:17 +0700

    completed

--- DeThiGiuaKi/De2/react_native_GK2/src/Screen4.jsx
+++ DeThiGiuaKi/De2/react_native_GK2/src/Screen4.jsx
@@ -7,8 +7,9 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 import productsData from "../productsData";
 
 const Screen4 = ({ navigation }) => {
@@ -16,9 +17,32 @@ const Screen4 = ({ navigation }) => {
 
   const [selectedSize, setSelectedSize] = useState(null); // State để lưu size được chọn
 
+  const [count, setCount] = useState(1); // State để lưu số lượng sản phẩm
+
+  const [total, setTotal] = useState(selectedProduct.price); // State để lưu tổng giá
+
   const handleSizeSelect = (size) => {
     setSelectedSize(size); // Cập nhật size khi được nhấn
   };
+
+  const handleTogglePlus = () => {
+    setCount((prevCount) => prevCount + 1);
+    setTotal((prevTotal) => (count + 1) * selectedProduct.price);
+  };
+
+  const handleToggleMinus = () => {
+    if (count > 1) {
+      setCount((prevCount) => prevCount - 1);
+      setTotal((prevTotal) => (count - 1) * selectedProduct.price);
+    }
+  };
+
+  // useEffect để cập nhật lại tổng giá mỗi khi sản phẩm được chọn thay đổi
+  useEffect(() => {
+    // Mỗi khi sản phẩm mới được chọn, cập nhật lại giá và đặt lại số lượng về 1
+    setCount(1); // Reset lại số lượng về 1
+    setTotal(selectedProduct.price); // Đặt lại tổng giá bằng giá của sản phẩm mới
+  }, [selectedProduct]);
 
   return (
     <ScrollView style={{ paddingVertical: 40, paddingHorizontal: 20 }}>
@@ -42,7 +66,7 @@ const Screen4 = ({ navigation }) => {
             <TouchableOpacity
               key={product.id}
               style={styles.viewImageRow}
-              onPress={() => setSelectedProduct(product)} // Update the selected product
+              onPress={() => setSelectedProduct(product)} // Cập nhật sản phẩm đã chọn
             >
               <Image source={product.image} />
             </TouchableOpacity>
@@ -71,7 +95,7 @@ const Screen4 = ({ navigation }) => {
       </View>
       {/* Size */}
       <View>
-        <Text style={[styles.textDescription, { fontSize: 24 }]}>Size</Text>
+        <Text style={styles.textTitleSize}>Size</Text>
         <View style={styles.viewSize}>
           {["XS", "S", "M", "L", "XL"].map((size) => (
             <TouchableOpacity
@@ -94,6 +118,63 @@ const Screen4 = ({ navigation }) => {
           ))}
         </View>
       </View>
+      {/* choose quantity */}
+      <View>
+        <Text style={[styles.textTitleSize, { marginVertical: 10 }]}>
+          Quantity
+        </Text>
+        <View style={styles.viewQuantity}>
+          <View style={styles.viewClickQuantity}>
+            <TouchableOpacity onPress={handleToggleMinus}>
+              <FontAwesome name="minus-circle" size={40} color="#00BDD6" />
+            </TouchableOpacity>
+            <Text style={styles.quantity}>{count}</Text>
+            <TouchableOpacity onPress={handleTogglePlus}>
+              <FontAwesome name="plus-circle" size={40} color="#00BDD6" />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.viewTotal}>
+            <Text style={styles.total}>Total</Text>
+            <Text style={styles.totalPrice}>${total.toFixed(2)}</Text>
+          </View>
+        </View>
+      </View>
+      {/* Size guide, Reviews */}
+      <>
+        <View style={styles.viewSizeGuide}>
+          <Text style={styles.textSizeGuide}>Size guide</Text>
+          <AntDesign
+            name="right"
+            size={22}
+            color="gray"
+            style={styles.textSizeGuideIconRight}
+          />
+        </View>
+        <View style={styles.viewSizeGuide}>
+          <Text style={styles.textSizeGuide}>Reviews (99)</Text>
+          <AntDesign
+            name="right"
+            size={22}
+            color="gray"
+            style={styles.textSizeGuideIconRight}
+          />
+        </View>
+      </>
+      {/* Button Add */}
+      <TouchableOpacity style={styles.buttonAdd}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 15,
+          }}
+        >
+          <AntDesign name="shoppingcart" size={24} color="white" />
+          <Text style={styles.textButton}>Add to Cart</Text>
+        </View>
+      </TouchableOpacity>
+      <View style={{ paddingBottom: 100 }}></View>
     </ScrollView>
   );
 };
@@ -180,11 +261,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginLeft: 10,
   },
-  textDescription: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#696969",
-    marginVertical: 10,
+  textTitleSize: {
+    fontSize: 22,
+    color: "#585858",
   },
   viewSize: {
     flexDirection: "row",
@@ -209,5 +288,72 @@ const styles = StyleSheet.create({
   },
   selectedTextSize: {
     color: "white", // Màu văn bản khi size được chọn
+  },
+  viewQuantity: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    paddingBottom: 25,
+  },
+  viewClickQuantity: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  quantity: {
+    fontSize: 28,
+    marginHorizontal: 20,
+    width: 35,
+    color: "#585858",
+    textAlign: "center",
+  },
+  viewTotal: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  total: {
+    fontSize: 20,
+    color: "#585858",
+  },
+  totalPrice: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#585858",
+    marginLeft: 10,
+  },
+  viewSizeGuide: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "rgba(0,0,0,0.3)",
+    paddingBottom: 25,
+  },
+  textSizeGuide: {
+    fontSize: 20,
+    color: "#585858",
+    justifyContent: "center",
+    marginTop: 20,
+    fontWeight: "bold",
+  },
+  textSizeGuideIconRight: {
+    fontSize: 20,
+    color: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+    marginTop: 24,
+  },
+  buttonAdd: {
+    backgroundColor: "#00BDD6",
+    width: 400,
+    height: 60,
+    borderRadius: 10,
+    marginTop: 40,
+    alignSelf: "center",
+  },
+  textButton: {
+    color: "white",
+    fontSize: 22,
+    marginLeft: 10,
   },
 });
