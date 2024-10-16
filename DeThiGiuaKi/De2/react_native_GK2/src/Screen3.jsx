eb30994689193407f0ef202f1d7814d92d5a1cae
Bao44 <tqbao44@gmail.com>
2024-10-16 11:50:19 +0700

    add screen3

--- DeThiGiuaKi/De2/react_native_GK2/src/Screen3.jsx
+++ DeThiGiuaKi/De2/react_native_GK2/src/Screen3.jsx
@@ -1,14 +1,107 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Fontisto from "@expo/vector-icons/Fontisto";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 const Screen3 = () => {
   return (
-    <View>
-      <Text>Screen3</Text>
+    <View style={{ paddingVertical: 40 }}>
+      <Image
+        source={require("../assets/Data/Image 20.png")}
+        style={{ width: "100%" }}
+      />
+      <View style={{ paddingHorizontal: 30 }}>
+        <Text style={styles.textHeader}>Welcome!</Text>
+        <Text style={styles.textInput}>Email</Text>
+        <View style={styles.viewInputEmail}>
+          <MaterialCommunityIcons
+            name="email-outline"
+            size={24}
+            color="gray"
+            style={{ paddingHorizontal: 10 }}
+          />
+          <TextInput
+            placeholder="Enter your email address"
+            // value={email}
+            // onChangeText={(newEmail) => setEmail(newEmail)}
+            style={{ paddingRight: 180, paddingLeft: 5 }}
+          />
+        </View>
+        <Text style={styles.textInput}>Password</Text>
+        <View style={styles.viewInputPass}>
+          <Fontisto
+            name="locked"
+            size={24}
+            color="gray"
+            style={{ paddingHorizontal: 10 }}
+          />
+          <TextInput
+            placeholder="Enter your password"
+            // value={password}
+            // onChangeText={(newPassword) => setPassword(newPassword)}
+            style={{ paddingRight: 180, paddingLeft: 5 }}
+          />
+          <MaterialCommunityIcons name="eye-outline" size={24} color="gray" />
+        </View>
+      </View>
+      <TouchableOpacity style={styles.buttonLogin}>
+        <Text style={styles.textButton}>Login</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default Screen3
+export default Screen3;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  textHeader: {
+    fontSize: 50,
+    fontWeight: "bold",
+    marginBottom: 50,
+    marginTop: 20,
+  },
+  textInput: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "gray",
+    marginVertical: 10,
+  },
+  viewInputEmail: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "gray",
+    paddingVertical: 12,
+    borderRadius: 15,
+    marginBottom: 30,
+  },
+  viewInputPass: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "gray",
+    paddingVertical: 12,
+    borderRadius: 15,
+    marginBottom: 30,
+  },
+  buttonLogin: {
+    backgroundColor: "#00BDD6",
+    width: 400,
+    height: 60,
+    borderRadius: 30,
+    marginTop: 40,
+    alignSelf: "center",
+  },
+  textButton: {
+    color: "white",
+    fontSize: 22,
+    textAlign: "center",
+    marginTop: 15,
+  },
+});
