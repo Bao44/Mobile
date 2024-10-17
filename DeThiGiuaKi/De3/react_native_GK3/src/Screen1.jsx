d51ee69c9bef0c84c7f6918a9d4f4993d284d6a1
Bao44 <tqbao44@gmail.com>
2024-10-17 20:04:24 +0700

    completed screen1

--- DeThiGiuaKi/De3/react_native_GK3/src/Screen1.jsx
+++ DeThiGiuaKi/De3/react_native_GK3/src/Screen1.jsx
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 
 const Screen1 = ({ navigation }) => {
   return (
     <View style={{ paddingVertical: 30 }}>
-      <Text>Screen1</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("Screen2")}>
-        <Text>Go to Screen2</Text>
+      <Text style={styles.textHeader}>Order your favorite!</Text>
+      <Image
+        source={require("../assets/data/soda.png")}
+        style={styles.imageDrink}
+      />
+      <Image
+        source={require("../assets/data/containerVegetable.jpg")}
+        style={styles.imageVegetable}
+      />
+      <Image
+        source={require("../assets/data/cua.jpg")}
+        style={styles.imageFood}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Screen2")}
+      >
+        <Text style={styles.textButton}>Get Started</Text>
       </TouchableOpacity>
     </View>
   );
@@ -14,4 +29,45 @@ const Screen1 = ({ navigation }) => {
 
 export default Screen1;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textHeader: {
+    fontSize: 36,
+    color: "#00CD66",
+    marginVertical: 30,
+    marginLeft: 20,
+  },
+  imageDrink: {
+    width: 200,
+    height: 200,
+    position: "absolute",
+    right: 0,
+    top: 130,
+  },
+  imageVegetable: {
+    width: 260,
+    height: 200,
+    position: "absolute",
+    left: 0,
+    top: 300,
+    left: 20,
+  },
+  imageFood: {
+    width: 260,
+    height: 200,
+    position: "absolute",
+    right: 20,
+    top: 530,
+  },
+  button: {
+    backgroundColor: "#00EE76",
+    paddingVertical: 14,
+    marginTop: 700,
+    marginHorizontal: 120,
+    alignItems: "center",
+    borderRadius: 50,
+  },
+  textButton: {
+    color: "#fff",
+    fontSize: 24,
+  },
+});
--- DeThiGiuaKi/De3/react_native_GK3/src/Screen2.jsx
+++ DeThiGiuaKi/De3/react_native_GK3/src/Screen2.jsx
@@ -1,14 +1,114 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
-const Screen2 = ({navigation}) => {
+const Screen2 = ({ navigation }) => {
+  const [selected, setSelected] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelected(option); // cập nhật giá trị selected
+  };
+
   return (
-    <View style={{ paddingVertical: 30 }}>
-      <Text>Screen2</Text>
+    <View style={{ paddingVertical: 40 }}>
+      <View style={styles.viewHeader}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Screen1")}
+          style={styles.button}
+        >
+          <AntDesign name="left" size={26} color="gray" />
+        </TouchableOpacity>
+        <FontAwesome5 name="shopping-cart" size={28} color="#00CD66" />
+      </View>
+      {/* Search */}
+      <View>
+        <View style={styles.viewTextInput}>
+          <TextInput
+            placeholder="Search"
+            style={{ padding: 10, fontSize: 18 }}
+          />
+        </View>
+      </View>
+      {/* Options */}
+      <View style={styles.viewOptions}>
+        {["Vegetable", "Seafood", "Meat"].map((option) => (
+          <TouchableOpacity
+            key={option}
+            style={[
+              styles.buttonOption,
+              selected === option && styles.selectedOption,
+            ]}
+            onPress={() => handleSelect(option)}
+          >
+            <Text
+              style={[
+                styles.textOptions,
+                selected === option && styles.textSelectedOption,
+              ]}
+            >
+              Vegetable
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      {/* Text */}
+      <View></View>
     </View>
-  )
-}
+  );
+};
 
-export default Screen2
+export default Screen2;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  viewHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 20,
+  },
+  button: {
+    alignSelf: "flex-start",
+    padding: 5,
+    borderRadius: 10,
+    backgroundColor: "rgba(0,0,0,0.07)",
+  },
+  viewTextInput: {
+    backgroundColor: "white",
+    marginHorizontal: 20,
+    borderRadius: 10,
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+  },
+  viewOptions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 30,
+    marginHorizontal: 20,
+  },
+  buttonOption: {
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 50,
+    borderWidth: 0.5,
+    borderColor: "rgba(0,0,0,0.2)",
+  },
+  textOptions: {
+    fontSize: 20,
+    paddingHorizontal: 10,
+    color: "#00CDCD",
+  },
+  selectedOption: {
+    backgroundColor: "#00CD66",
+  },
+  textSelectedOption: {
+    color: "white",
+  },
+});
